61a7d504c218cacae1c94641ef340c7a3649b009
baichuangbaijia <924481553@qq.com>
2026-05-26 03:24:36 +0800

    v2.0.61: WKWebView+HTML5 Canvas office, lobsters sit at desks with screen content

--- ChuangpuAI/Views/Home/LobsterOfficeView.swift
+++ ChuangpuAI/Views/Home/LobsterOfficeView.swift
@@ -1,16 +1,24 @@
 import SwiftUI
-import SpriteKit
+import WebKit
 
+/// 虚拟办公室 v11 - WKWebView + HTML5 Canvas
 struct LobsterOfficeView: UIViewRepresentable {
-    func makeUIView(context: Context) -> SKView {
-        let view = SKView()
-        view.backgroundColor = UIColor(hex: "0D0D1A")
-        view.ignoresSiblingOrder = true
-        view.isMultipleTouchEnabled = false
-        let scene = LobsterOfficeScene(size: CGSize(width: UIScreen.main.bounds.width, height: 360))
-        scene.scaleMode = .aspectFill
-        view.presentScene(scene)
-        return view
+    func makeUIView(context: Context) -> WKWebView {
+        let config = WKWebViewConfiguration()
+        config.preferences.javaScriptEnabled = true
+        let web = WKWebView(frame: .zero, configuration: config)
+        web.isOpaque = false
+        web.backgroundColor = UIColor(red: 13/255, green: 13/255, blue: 26/255, alpha: 1)
+        web.scrollView.isScrollEnabled = false
+        web.scrollView.bounces = false
+        
+        // 加载本地HTML
+        if let url = Bundle.main.url(forResource: "lobster_office", withExtension: "html") {
+            web.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent())
+        } else if let path = Bundle.main.path(forResource: "lobster_office", ofType: "html") {
+            web.load(URLRequest(url: URL(fileURLWithPath: path)))
+        }
+        return web
     }
-    func updateUIView(_ uiView: SKView, context: Context) {}
+    func updateUIView(_ uiView: WKWebView, context: Context) {}
 }
